f6ad328019bbbdbd82aaf3531cc03272109e5faa
Максим Федотов <fedotom2@gmail.com>
2020-10-24 21:29:49 +0300

    Init project

---
+++ app.js
@@ -0,0 +1,32 @@
+'use strict';
+
+const express = require('express');
+const mongoose = require('mongoose');
+const config = require('config');
+const bodyParser = require('body-parser');
+const api = require('./api.js');
+const app = express();
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({
+  extended: true
+}));
+
+app.use('/', api);
+
+const start = async () => {
+  try {
+    await mongoose.connect(config.get('MONGOURI'), {
+      useNewUrlParser: true,
+      useUnifiedTopology: true
+    });
+
+    const PORT = config.get('PORT') || 8000;
+    app.listen(PORT, () => console.log(`Listening on port ${ PORT }`));
+  } catch (err) {
+    console.error(err.message);
+    process.exit(1);
+  }
+};
+
+start();
